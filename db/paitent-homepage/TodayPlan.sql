42ee36d616b84bc77c0f0bd10ece207e72dacba4
shanimic <micshani9@gmail.com>
2026-04-14 17:15:53 +0300

    feat: add TodayPlan SQL query to retrieve today's exercises for active sessions

---
+++ db/paitent-homepage/TodayPlan.sql
@@ -0,0 +1,10 @@
+select pe.exercise_id,e.exercise_name,  e.visit_type,pe.reps,ec.execution_status
+    from weekly_plans wp , plan_exercises pe, exercises e,exercise_completion ec
+where wp.session_id = pe.session_id
+  and pe.exercise_id = e.exercise_id
+  and pe.session_id = ec.session_id
+  and wp.exercise_date= CURDATE()
+and pe.session_id in 
+(select s.session_id
+    from sessions s where s.patient_id = 'P100'
+         and s.session_status = 'ACTIVE' );
