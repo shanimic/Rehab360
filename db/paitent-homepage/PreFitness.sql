d7be1f9181d8785bfe9714d17ac11352da6b0dcb
Shani Michaeli <shanimi2@mta.ac.il>
2026-04-16 22:09:50 +0300

    feat: add SQL scripts for managing user registrations, exercises, ses… (#66)
    * feat: add SQL scripts for managing user registrations, exercises, sessions, and weekly plans
    
    * refactor: remove unused SQL queries for fitness and physiotherapist completion
    
    * refactor: remove unused SQL queries and streamline exercise completion calculations

---
+++ db/paitent-homepage/PreFitness.sql
@@ -0,0 +1,19 @@
+-- precentage of fitness completion
+select (EXE_COMPLETED.EXECOMP / (NUM_EXE_PER_W.NEPW * NUM_WEEKS.weeks_diff)) * 100 
+from 
+(select sum( pe.reps * pe.num_sets * pe.time_duration * 
+(case pe.time_unit when'Weekly'then 1 else 7 end )) as NEPW
+from plan_exercises pe,sessions s
+where pe.session_id = s.session_id and s.patient_id =?
+and s.session_status = 'ACTIVE'
+and s.visit_type = 'FITNESS') as NUM_EXE_PER_W,
+(SELECT TIMESTAMPDIFF(WEEK, p.start_date , p.end_date) AS weeks_diff 
+from plans p, sessions s
+where p.session_id = s.session_id and s.patient_id =?
+and s.session_status = 'ACTIVE'
+and s.visit_type = 'FITNESS') as NUM_WEEKS,
+(select sum(ec.num_exe_completed) as EXECOMP from exercise_completion ec,sessions s
+where ec.session_id = s.session_id and s.patient_id =?
+and s.session_status = 'ACTIVE'
+and s.visit_type = 'FITNESS') as EXE_COMPLETED 
+;
--- db/paitent-homepage/TodayPlan.sql
+++ db/paitent-homepage/TodayPlan.sql
@@ -1,3 +1,4 @@
+-- list of today exercies with status of execution 
 select pe.exercise_id,e.exercise_name,  e.visit_type,pe.reps,ec.execution_status
     from weekly_plans wp , plan_exercises pe, exercises e,exercise_completion ec
 where wp.session_id = pe.session_id
@@ -6,5 +7,6 @@ where wp.session_id = pe.session_id
   and wp.exercise_date= CURDATE()
 and pe.session_id in 
 (select s.session_id
-    from sessions s where s.patient_id = 'P100'
+    from sessions s where s.patient_id =?
          and s.session_status = 'ACTIVE' );
+
