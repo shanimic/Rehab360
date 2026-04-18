26a4aeec36576361bf70a88de2b6c4bb618f2585
Shani Michaeli <shanimi2@mta.ac.il>
2026-04-18 14:13:14 +0300

    fix: add patient_id filter to SQL queries for accurate completion percentage (#67)

--- db/paitent-homepage/PrePyshio.sql
+++ db/paitent-homepage/PrePyshio.sql
@@ -3,17 +3,17 @@ select (EXE_COMPLETED.EXECOMP / (NUM_EXE_PER_W.NEPW * NUM_WEEKS.weeks_diff)) * 1
 from 
 (select sum( pe.reps * pe.num_sets * pe.time_duration * 
 (case pe.time_unit when'Weekly'then 1 else 7 end )) as NEPW
-from plan_exercises pe,sessions s
-where pe.session_id = s.session_id
+from plan_exercises pe,sessions s 
+where pe.session_id = s.session_id and s.patient_id =?
 and s.session_status = 'ACTIVE'
 and s.visit_type = 'PHYSIOTHERAPIST') as NUM_EXE_PER_W,
 (SELECT TIMESTAMPDIFF(WEEK, p.start_date , p.end_date) AS weeks_diff 
 from plans p, sessions s
-where p.session_id = s.session_id
+where p.session_id = s.session_id and s.patient_id =?
 and s.session_status = 'ACTIVE'
 and s.visit_type = 'PHYSIOTHERAPIST') as NUM_WEEKS,
 (select sum(ec.num_exe_completed) as EXECOMP from exercise_completion ec,sessions s
-where ec.session_id = s.session_id
+where ec.session_id = s.session_id and s.patient_id =?
 and s.session_status = 'ACTIVE'
 and s.visit_type = 'PHYSIOTHERAPIST') as EXE_COMPLETED 
 ;
